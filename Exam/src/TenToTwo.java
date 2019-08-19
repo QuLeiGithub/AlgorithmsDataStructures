cbac4460b14c5919ae9b72524ee8bc059942c59d
qulei <qulei_20180331@163.com>
2019-08-20 00:33:29 +0800

    ADD | 加入了一个十进制转二进制的方法

---
+++ Exam/src/TenToTwo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * 将十进制转化为二进制
+ *
+ * @author QuLei
+ */
+public class TenToTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个十进制数：");
+        int number = sc.nextInt();
+        String str = "";
+        while (number != 0) {
+            int i = number % 2;
+            str = i + str;
+            number = number / 2;
+        }
+        System.out.println(str);
+    }
+}
