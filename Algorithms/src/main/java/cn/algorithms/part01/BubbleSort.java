a92e7828edd4c265b95d8eb5c43f7d4d8c27eba6
qulei <qulei_20180331@163.com>
2020-04-18 22:05:03 +0800

    Add:增加冒泡排序实现，和对数器测试

--- Algorithms/src/main/java/cn/algorithms/part01/BubbleSort.java
+++ Algorithms/src/main/java/cn/algorithms/part01/BubbleSort.java
@@ -1,65 +1,58 @@
 package cn.algorithms.part01;
 
-
 import java.util.Arrays;
 
 /**
+ * 测试冒泡排序 时间复杂度为O(n^2)
+ *
  * @author TaoistQu
  */
-public class SelectionSort {
-    /**
-     * 选择排序
-     * 时间复杂度 O(n^2)
-     *
-     * @param arr
-     */
-    private static void selectionSort(int[] arr) {
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
-        for (int i = 0; i < arr.length - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+        for (int e = arr.length - 1; e > 0; e--) {
+            for (int i = 0; i < e; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
             }
-            swap(arr, minIndex, i);
         }
     }
 
     /**
-     * 交换数组两位置元素
+     * 交换数组两位置上的值
      *
      * @param arr
      * @param i
      * @param j
      */
     private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
     }
 
     public static void main(String[] args) {
         int testTime = 500000;
-        int maxSize = 100;
         int maxValue = 100;
+        int maxSize = 100;
         boolean succeed = true;
         for (int i = 0; i < testTime; i++) {
             int[] arr1 = ArrayUtil.generateRandomArray(maxSize, maxValue);
             int[] arr2 = ArrayUtil.copyArray(arr1);
-            selectionSort(arr1);
+            bubbleSort(arr1);
             ArrayUtil.comparator(arr2);
             if (!ArrayUtil.isEqual(arr1, arr2)) {
                 succeed = false;
-                ArrayUtil.printArray(arr1);
-                ArrayUtil.printArray(arr2);
                 break;
             }
         }
-        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+        System.out.println(succeed ? "Nice" : "Fucking fucked!");
         int[] arr = ArrayUtil.generateRandomArray(maxSize, maxValue);
         ArrayUtil.printArray(arr);
-        selectionSort(arr);
+        bubbleSort(arr);
         ArrayUtil.printArray(arr);
     }
 }
