f454e78328987cbb019068b3cd508f3d76e361dd
qulei <qulei_20180331@163.com>
2020-04-20 00:25:04 +0800

    Add:增加归并排序

---
+++ Algorithms/src/main/java/cn/algorithms/part03/MergeSort.java
@@ -0,0 +1,108 @@
+package cn.algorithms.part03;
+
+import cn.algorithms.part01.util.ArrayUtil;
+
+/**
+ * 归并排序
+ *
+ * @author TaoistQu
+ */
+public class MergeSort {
+    /**
+     * 递归方式实现
+     *
+     * @param arr
+     */
+    public static void mergeSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+    public static void process(int[] arr, int L, int R) {
+        if (L == R) {
+            return;
+        }
+        int mid = L + ((R - L) >> 1);
+        process(arr, L, mid);
+        process(arr, mid + 1, R);
+        merge(arr, L, mid, R);
+    }
+
+    /**
+     * 非递归版本
+     *
+     * @param arr
+     */
+    public static void mergeSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        int mergeSize = 1;
+        while (mergeSize < N) {
+            int L = 0;
+            while (L < N) {
+                int M = L + mergeSize - 1;
+                if (M >= N) {
+                    break;
+                }
+                int R = Math.min(M + mergeSize, N - 1);
+                merge(arr, L, M, R);
+                L = R + 1;
+            }
+            if (mergeSize > N / 2) {
+                break;
+            }
+            mergeSize <<= 1;
+        }
+    }
+
+    /**
+     * 合并数组
+     *
+     * @param arr
+     * @param L
+     * @param M
+     * @param R
+     */
+    public static void merge(int[] arr, int L, int M, int R) {
+        int[] help = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = M + 1;
+        while (p1 <= M && p2 <= R) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= M) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[L + i] = help[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = ArrayUtil.generateRandomArray(maxSize, maxValue);
+            int[] arr2 = ArrayUtil.copyArray(arr1);
+            mergeSort1(arr1);
+            mergeSort2(arr2);
+            if (!ArrayUtil.isEqual(arr1, arr2)) {
+                succeed = false;
+                ArrayUtil.printArray(arr1);
+                ArrayUtil.printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Oops!");
+    }
+}
